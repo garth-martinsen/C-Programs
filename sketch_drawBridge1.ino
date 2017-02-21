ce89acb3a92385716892a6a8f7f538d6b9353068
Garth Martinsen <garth.martinsen@gmail.com>
2017-02-21 11:29:24 -0800

    Removed redundant lines of code

--- sketch_drawBridge1.ino
+++ sketch_drawBridge1.ino
@@ -7,42 +7,47 @@ int dir = 5;    // direction Relay HIGH -> up;  LOW -> down.
 int motor = 6;  // motor power relay
 
 double longestPeriod=20000.0;
-volatile double loopPeriod= longestPeriod;  //delay will approach 0 as bridge nears max height.
+volatile double loopPeriod= longestPeriod;  //delay will approach 100 ms as bridge nears limit switches.
 
-int debounceTime=20; //20ms
 
-// boolean values set by the ISRs and used to control actuators.
+
+// boolean values from reading sensors
 volatile boolean shouldRise;
 volatile boolean atHigh;
 volatile boolean shouldLower;
 volatile boolean atLow;
+
+//actuator values
 volatile boolean direkshun;
 volatile boolean motorsOn;
+
+//Bridge States
 volatile boolean isRising=false;
 volatile boolean isLowering=false;
-volatile boolean stopRise=false;
-volatile boolean stopLower=false;
 volatile boolean bridgeUp=false;
 volatile boolean bridgeDown=false;
 
 
+
 void readSensors(){
   //Serial.print("  Reading digital inputs...\n");
-  shouldRise =   !digitalRead(wu);
-  atHigh =   !digitalRead(he);
-  shouldLower = !digitalRead(wd);
-  atLow =    !digitalRead(le);
-  direkshun = !digitalRead(dir);
-  motorsOn = !digitalRead(motor);
- 
-
- 
+  shouldRise  =   !digitalRead(wu);
+  atHigh      =   !digitalRead(he);
+  shouldLower =   !digitalRead(wd);
+  atLow       =   !digitalRead(le);
+  direkshun   =   !digitalRead(dir);
+  motorsOn    =   !digitalRead(motor);
 }
+
 void takeAction(){
-  if( shouldRise && !isRising && !atHigh) watersUp();
-  else if(atHigh && isRising) highEnough();
-  else if(!atLow && shouldLower && !isLowering) watersDown();
-  else if(atLow && isLowering) lowEnough();
+  if( shouldRise && !isRising && !atHigh) 
+    watersUp();
+  else if(atHigh && isRising) 
+    highEnough();
+  else if( shouldLower && !isLowering && !atLow ) 
+    watersDown();
+  else if(atLow && isLowering) 
+    lowEnough();
   else Serial.print("No Action.");
 }
  
@@ -53,89 +58,74 @@ void updatePeriod(){
 }
 void showStatus(){
   Serial.print("loopPeriod: " + String(loopPeriod) + " Bridge is : " + bridgeStatus() + "  " +
-               String(shouldRise) + "," + 
-               String(atHigh) + "," + 
-               String(shouldLower) + "," + 
-               String(atLow)+ "," + 
-               String(digitalRead(dir))+ "," + 
-               String(digitalRead(motor))+"\n");
+         String(shouldRise) + "," + 
+         String(atHigh) + "," + 
+         String(shouldLower) + "," + 
+         String(atLow)+ "," + 
+         String(digitalRead(dir))+ "," + 
+         String(digitalRead(motor))+"\n");
 }
 
 
 void watersUp(){  //called when high water switch indicates that bridge shouldRise.
-   Serial.print("watersUp function called. Started raising the bridge.\n");
+   //Serial.print("watersUp function called. Started raising the bridge.\n");
     if(!isRising)   isRising   = !atHigh && shouldRise;
-    if(isRising) startRaising();
+    Serial.print("Setting the motor direction:up and motor power:on. Raising the bridge\n");
+    digitalWrite(dir,HIGH); //up direction
+    digitalWrite(motor,HIGH);
+    loopPeriod = longestPeriod; 
 }
 
-  //called when maximum height switch is activated. Set brakes, deactivate relay, stop motor.
+  //called when maximum height switch is closed. Set brakes, deactivate relay, stop motor.
 void highEnough(){ 
-  Serial.print("highEnough function called.Stopped raising the bridge.\n");
-  if(!stopRise)   stopRise   =  atHigh && isRising;
-  if(stopRise) stopRaising();
-  bridgeUp = true;
-  bridgeDown=false;
-  isRising=false;
-  loopPeriod = longestPeriod;
-  
+ // Serial.print("highEnough function called.\n");
+    Serial.print("Setting motor power: off. Stopped raising the bridge\n");
+    digitalWrite(motor,LOW); 
+    loopPeriod=longestPeriod;
+    bridgeUp = true;
+    bridgeDown=false;
+    isRising=false;
+    loopPeriod = longestPeriod;
 }
 
 //called when water is low enough to lower bridge. Activates relay to start motor to lower the bridge
 void watersDown(){ 
-     Serial.print("watersDown function called. Started lowering the bridge.\n");
-   if(!isLowering) isLowering = !atLow && shouldLower;
-   if(isLowering) startLowering();
+  // Serial.print("watersDown function called. Started lowering the bridge.\n");
+    if(!isLowering) isLowering = !atLow && shouldLower;
+    Serial.print("Setting motor direction:down and motor power: on. Lowering the bridge.\n");
+    digitalWrite(dir,LOW); //direction: down.
+    digitalWrite(motor,HIGH); //start motor
+    loopPeriod = longestPeriod; 
 }
 
-// called when minimum height switch is activated.Deactivate relay, stopping the motor
+// called when minimum height switch is closed. Deactivates relay, stopping the motor
 void lowEnough(){  
-     Serial.print("lowEnough function called.Stopped bridge lowering.\n");
-       if(!stopLower)  stopLower  =  atLow && shouldLower;
-       if(stopLower) stopLowering();
+  //   Serial.print("lowEnough function called.");
+       Serial.print("Setting motor power: OFF. Stopped lowering bridge.\n");
+       digitalWrite(motor,LOW);
+       loopPeriod = longestPeriod;
+       isLowering = false; 
        bridgeUp=false;
        bridgeDown=true;
- 
 }
-
-void startRaising(){
-  Serial.print("Setting the motor direction:up and motor power:on.\n");
-  digitalWrite(dir,HIGH); //up direction
-  digitalWrite(motor,HIGH);
-  loopPeriod = longestPeriod; 
-}
-
-void stopRaising(){
-  Serial.print("Setting motor power: off.");
-  digitalWrite(motor,LOW); 
-  loopPeriod=longestPeriod;
-}
- void   startLowering(){
-  Serial.print("Setting motor direction:down and motor power: on.\n");
-  digitalWrite(dir,LOW); //direction: down.
-  digitalWrite(motor,HIGH); //start motor
-  loopPeriod = longestPeriod; 
- }
- void stopLowering(){
-  Serial.print("Setting motor power: OFF.\n");
-  digitalWrite(motor,LOW);
-  loopPeriod = longestPeriod;
-  isLowering = false; 
-  bridgeUp=false;
-  bridgeDown=true;
- }
 
  String bridgeStatus(){
-  String stat;
- if(isRising) stat="Rising";
- else if(isLowering) stat= "Lowering";
- else if(bridgeUp) stat="Raised";
- else if(bridgeDown) stat="Lowered";
- else stat= "Waiting for Events!";
+   String stat;
+   if(isRising) 
+    stat="Rising";
+   else if(isLowering) 
+    stat= "Lowering";
+   else if(bridgeUp) 
+    stat="Raised";
+   else if(bridgeDown) 
+    stat="Lowered";
+   else 
+    stat= "Waiting for Events!";
  return stat + " ";
  }
 
 void setup() {
-  // put your setup code here, to run once:
+  //set pins as input or output, pullup so quiescent state is all HIGH.
   Serial.begin(9600);
   pinMode(wu, INPUT);
   pinMode(he, INPUT);
